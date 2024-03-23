0e6e55f7e77ff3c03a04ac1853fc09ca11f575f0
brian-ambani <brainambani1@gmail.com>
2024-03-24 00:34:16 +0300

    add the optimal solution fro anagram

--- anagram.js
+++ anagram.js
@@ -3,11 +3,36 @@
 // Input: listen, silent --->>>> Output: true
 // Input: hello, hola --->>>> Output: false
 
-const isAnagram = (str1, str2) => {
-  str1 = str1.split("").sort().join("");
-  str2 = str2.split("").sort().join("");
+// Firs Approach
+// const isAnagram = (str1, str2) => {
+//   str1 = str1.split("").sort().join("");
+//   str2 = str2.split("").sort().join("");
 
-  return str1 === str2;
+//   return str1 === str2;
+// };
+
+// const result = isAnagram("listen", "silent");
+
+// console.log(result);
+
+// Second Approach
+
+const isAnagram = (str1, str2) => {
+  if (str1.length !== str2.length) return false;
+
+  let obj1 = {};
+  let obj2 = {};
+
+  for (let i = 0; i < str1.length; i++) {
+    obj1[str1[i]] = (obj1[str1[i]] || 0) + 1;
+    obj2[str2[i]] = (obj2[str2[i]] || 0) + 1;
+  }
+
+  for (const key in obj1) {
+    if (obj1[key] !== obj2[key]) return false;
+  }
+
+  return true;
 };
 
 const result = isAnagram("listen", "silent");
