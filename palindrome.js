d4dd34ca0d33e2c34cc0142634e8e4806527eb36
brian-ambani <brainambani1@gmail.com>
2024-03-23 13:52:49 +0300

    add clean code for palindrome number

--- palindrome.js
+++ palindrome.js
@@ -2,10 +2,10 @@
 // Given an integer x, return true if x is palindrome integer.
 // An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
 
-function ispalindrome(x) {
+const isPalindrome = function (x) {
   return x < 0 ? false : x === +x.toString().split("").reverse().join("");
-}
+};
 
-const result = ispalindrome(-121);
+const result = isPalindrome(121);
 
 console.log(result);
