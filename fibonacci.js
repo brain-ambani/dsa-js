122fca4061921078585aff8997b9d2f40a353f66
brian-ambani <brainambani1@gmail.com>
2024-03-23 14:11:16 +0300

    fibonacci number usin g a forloop

---
+++ fibonacci.js
@@ -0,0 +1,20 @@
+// Fibonacci Number
+// The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is:
+// Fibonacci Series: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+// F(0) = 0, F(1) = 1
+// F(n) = F(n - 1) + F(n - 2), for n > 1
+
+// Input: n = 3 --->>>> Output: 2
+
+// Fibonacci Series using a for loop
+
+const fib = function (n) {
+  const arr = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    arr.push(arr[i - 1] + arr[i - 2]);
+  }
+  console.log(arr[n]);
+};
+
+fib(7);
